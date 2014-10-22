ee93e2f1fe22e46d930f75dcb449b2454a6d7548
Arjun S Kumar <arjun@brawnycode.com>
2014-10-22 15:30:02 +0530

    Update landing page

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,800,700,600,300' rel='stylesheet' type='text/css'>
+    <meta charset="UTF-8">
+    <title>Made with &hearts; in India</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style type="text/css">
+    	body {
+    		background: #e74c3c;
+    		margin: 0;
+    		padding: 0;
+    		font-family: 'Open Sans', sans-serif;
+    	}
+    	#wrapper {
+    		max-width: 782px;
+    		margin: 50px auto;
+    		text-align: center;
+    		color: #fff;
+    	}
+    	#wrapper h1 {
+			line-height: 1em;
+			color: #fff;
+			font-size: 109px;
+			text-shadow:0px 0px 0 rgb(228,228,228),0px 1px 0 rgb(210,210,210),0px 2px 0 rgb(193,193,193),0px 3px 0 rgb(175,175,175),0px 4px 0 rgb(158,158,158), 0px 5px 0 rgb(141,141,141),0px 6px 5px rgba(0,0,0,0.6),0px 6px 1px rgba(0,0,0,0.5),0px 0px 5px rgba(0,0,0,.2);
+    	}
+    	#wrapper span {
+    		color: #fff9d6;
+    		font-size: 50px;
+    		font-style:italic;
+            margin-top: -30px;
+    	}
+    	#wrapper span.madeby a {
+    		margin-top: 50px;
+    		font-size: 14px;
+            color: #545454;
+    	}
+    </style>
+</head>
+<body>
+	<section id="wrapper">
+		<h1>We're building something cool!</h1>
+		<span>We'll reach out to you soon!</span>
+		<footer>
+			<span class="madeby">
+				<a href="http://madewithloveinindia.org/" title="Made with love in India">Made with &hearts; in India.</a>
+			</span>
+		</footer>
+
+	</section>
+</body>
+</html>
