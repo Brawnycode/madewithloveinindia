e7631d66733de644b1a78053efab8156256e2245
Arjun S Kumar <arjun@brawnycode.com>
2014-10-23 09:11:42 +0530

    Move CSS from head to file

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,800,700,600,300' rel='stylesheet' type='text/css'>
+    <meta charset="UTF-8">
+    <title>Made with &hearts; in India</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css" media="all">
+</head>
+<body>
+	<section id="wrapper">
+		<h1>We're building something cool!</h1>
+		<span>We'll reach out to you soon!</span>
+		<footer>
+			<span class="madeby">
+				<a href="http://madewithloveinindia.org/" title="Made with love in India">Made with &hearts; in India.</a>
+			</span>
+		</footer>
+
+	</section>
+</body>
+</html>
